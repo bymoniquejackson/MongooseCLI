4803106eb6c7705b0a864c37d12c3d921f8ecfa2
MJ <100384634+bymoniquejackson@users.noreply.github.com>
2022-06-01 11:10:20 +0100

    Update model.js

--- src/movie/model.js
+++ src/movie/model.js
@@ -1,26 +1,24 @@
 const mongoose = require("mongoose");
 
-
 const movieSchema = new mongoose.Schema({
-    title: {
-        type: String, 
-        required: true,
-        unique: true,
-    },
-    actor: {
-        type: String,
-        default: "Not Specified",
-    },
-    rating: {
-        type: Number, 
-    },
-    year: {
-        type: String,
-        default: "Not Specified",
-    }
+	title: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+
+
+	actor: {
+		type: String,
+		default: "Not specified",
+	},
+
+	director: {
+		type: String,
+		default: "Not specified",
+	},
 });
-// const User = mongoose.model("User")
+
 const Movie = mongoose.model("Movie", movieSchema);
 
 module.exports = Movie;
-
